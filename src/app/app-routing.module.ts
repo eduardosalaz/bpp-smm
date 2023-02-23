268f3747fb71a6101fb03d658ea67fb79bc742f8
Eduardo Salazar <eduardosalaz@outlook.com>
2023-02-23 13:16:29 -0600

    Componentes básicos de las paginas iniciales, ruteo establecido, añadir Angular Material.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TutorialComponent} from "./components/tutorial/tutorial.component";
+import {LandingComponent} from "./components/landing/landing.component";
+import {OnlineComponent} from "./components/online/online.component";
+import {OfflineComponent} from "./components/offline/offline.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'tutorial',
+    component: TutorialComponent,
+  },
+  {
+    path: 'online',
+    component: OnlineComponent,
+  },
+  {
+    path: 'offline',
+    component: OfflineComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import { TutorialComponent } from './components/tutorial/tutorial.component';
+import { OnlineComponent } from './components/online/online.component';
+import { OfflineComponent } from './components/offline/offline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TutorialComponent,
+    OnlineComponent,
+    OfflineComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
